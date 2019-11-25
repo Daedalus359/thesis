8da88794fb8a4ad8165038a8e9423da54831a075
= <=>
2019-11-25 15:43:31 -0800

    spell checking utilities

--- spellCheck.sh
+++ spellCheck.sh
@@ -2,6 +2,6 @@
 
 echo "Checking Spelling"
 
-aspell --mode=tex -c --ignore-case --personal jargon.txt Simulation_Development.tex
+aspell --home-dir=. --mode=tex -c --ignore-case --personal=jargon.txt Simulation_Development.tex
 
 echo "Spell check complete."
