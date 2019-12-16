5d6ad564d237d3b7f5c747cc70c30dbab286c321
kevin <=>
2019-12-16 11:27:10 -0500

    spell checking and more minor modules for 2

--- spellCheck.sh
+++ spellCheck.sh
@@ -2,6 +2,12 @@
 
 echo "Checking Spelling"
 
-aspell --home-dir=. --mode=tex -c --ignore-case --personal=jargon.txt Simulation_Development.tex
+OPTIONS="--home-dir=. --mode=tex -c --ignore-case --personal=jargon.txt"
+
+echo "Chapter 1 Spelling"
+aspell $OPTIONS 1_Introduction.tex
+
+echo "Chapter 2 Spelling"
+aspell $OPTIONS 2_Simulation_Development.tex
 
 echo "Spell check complete."
