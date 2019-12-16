b5135f8edeff4e97bfc31d237018a5588bca4c72
kevin <=>
2019-12-16 11:33:33 -0500

    yet more spell checks

--- spellCheck.sh
+++ spellCheck.sh
@@ -4,10 +4,22 @@ echo "Checking Spelling"
 
 OPTIONS="--home-dir=. --mode=tex -c --ignore-case --personal=jargon.txt"
 
-echo "Chapter 1 Spelling"
+echo "Chapter 1"
 aspell $OPTIONS 1_Introduction.tex
 
-echo "Chapter 2 Spelling"
+echo "Chapter 2"
 aspell $OPTIONS 2_Simulation_Development.tex
+
+echo "Chapter 3"
+aspell $OPTIONS 3_Offline_Planning_Policies.tex
+
+echo "Chapter 4"
+aspell $OPTIONS 4_Online_Planning_Policies.tex
+
+echo "Chapter 5"
+aspell $OPTIONS 5_Conclusion.tex
+
+echo "Other TeX Files"
+aspell $OPTIONS 0_FrontMatter.tex
 
 echo "Spell check complete."
