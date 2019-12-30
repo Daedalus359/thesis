1139741afc9fd6077fb0bb39625071259b555520
kevin <=>
2019-12-30 11:42:44 -0500

    title change and related file changes

--- buildThesis.sh
+++ buildThesis.sh
@@ -2,10 +2,10 @@
 
 echo "Building PDF"
 
-pdflatex --shell-escape Online_Planning_for_Robotic_Coverage.tex
+pdflatex --shell-escape Variable_Scrutiny_Robotic_Coverage.tex
 
-if biber Online_Planning_for_Robotic_Coverage; then
-	pdflatex --shell-escape Online_Planning_for_Robotic_Coverage.tex 
-	pdflatex --shell-escape Online_Planning_for_Robotic_Coverage.tex; else
+if biber Variable_Scrutiny_Robotic_Coverage; then
+	pdflatex --shell-escape Variable_Scrutiny_Robotic_Coverage.tex
+	pdflatex --shell-escape Variable_Scrutiny_Robotic_Coverage.tex; else
 	echo "Build Failed at biber"
 fi
